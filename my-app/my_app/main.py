47fa4ad98881687fe7acb2f36f2f7f8c6dfbde09
unknown <yskim23489@gmail.com>
2024-09-10 10:14:14 +0900

    #1 myapp

---
+++ my-app/my_app/main.py
@@ -0,0 +1,9 @@
+def add(a:int, b: int) -> int;
+    return a + b;
+
+
+    if __name__== "__main__"
+        result = add(2,3)
+        print(f"2 + 3 = {result}")
+
+        
---
+++ my-app/tests/test_main.py
@@ -0,0 +1,7 @@
+from my_app.main import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(2,5) == 1000
